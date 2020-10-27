d7ccf137474abae0cc2280f0ce7c053b896ac3ab
millywayne <achiengmillicent35@gmail.com>
2020-10-27 17:43:17 +0300

    added upvote and downvote functions

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,29 +1,29 @@
 from flask import render_template, request, redirect, url_for
-from login import login_user, logout_user
+from flask_login import login_user,login_required,logout_user
 from app.auth import auth
 from app.models import User
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        form = request.form
-        username = form.get('username')
-        password = form.get('password')
-        print(username)
-        user = User.query.filter_by(username=username).first()
-        if user is None:
-            error = 'None exixstant Username'
-            return render_template('login.html', error=error)
-        is_correct_password = user.check_password(password)
-        print(is_correct_password)
-        if not is_correct_password:
-            error = 'Non existant Password'
+      if request.method != 'POST':
+            return render_template('login.html')
+      form = request.form
+      username = form.get('username')
+      password = form.get('password')
+      print(username)
+      user = User.query.filter_by(username=username).first()
+      if user is None:
+          error = 'None exixstant Username'
+          return render_template('login.html', error=error)
+      is_correct_password = user.check_password(password)
+      print(is_correct_password)
+      if not is_correct_password:
+          error = 'Non existant Password'
 
 
-            return render_template('login.html', error=error)
-        login_user(user)
-        return redirect('/')
-    return render_template('login.html')
+          return render_template('login.html', error=error)
+      login_user(user)
+      return redirect('/')
 
 
 @auth.route('/signup', methods=['GET', 'POST'])
